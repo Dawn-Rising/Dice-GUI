c38c9e66143240e0efe45ad751b96ce1494c5c55
Dawn-Rising <105828986+Dawn-Rising@users.noreply.github.com>
2022-05-19 11:58:06 +0100

    Add files via upload

---
+++ Dice game/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Garamond", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(22, 162)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(478, 54)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Roll!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(5, 228)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(207, 85)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Score: 0"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Dice_game.My.Resources.Resources.Dice1
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(156, 156)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(218, 228)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(290, 85)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Highest Roll: 0"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Dice_game.My.Resources.Resources.Dice1
+        Me.PictureBox2.Location = New System.Drawing.Point(174, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(156, 156)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Dice_game.My.Resources.Resources.Dice1
+        Me.PictureBox3.Location = New System.Drawing.Point(344, 0)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(156, 156)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei UI", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(9, 329)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(478, 91)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "High Score: 0"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(537, 445)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Dice"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Dice game/Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+    Public score As Integer = 0
+    Public High_roll As Integer = 0
+    Function save(score As Integer)
+        Dim filePath As String = "C:\Users\Aurora\OneDrive - Calderdale College\github sort folder\Computing\Dice game\High score.txt"
+        Dim highscore As Integer
+        Dim fileHolder As System.IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(filePath)
+
+        Dim line As String = fileHolder.ReadLine()
+
+
+
+        ' If line < score Then
+
+        '     My.Computer.FileSystem.WriteAllText(filePath, score, False)
+        'highscore = score
+        'Else
+        'highscore = line
+        'End If
+
+        Return (highscore)
+    End Function
+    Sub endscript()
+        Dim terminate As Boolean = False
+        While terminate = False
+            Button1.Name = "Game Over"
+        End While
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim randomNumber As Integer
+        Randomize()
+        randomNumber = Int(Rnd() * 6 + 1)
+
+        Dim scoretemp As Integer = 0
+
+
+        If randomNumber = 1 Then
+            PictureBox1.Image = My.Resources.Dice1
+        ElseIf randomNumber = 2 Then
+            PictureBox1.Image = My.Resources.Dice2
+        ElseIf randomNumber = 3 Then
+            PictureBox1.Image = My.Resources.Dice3
+        ElseIf randomNumber = 4 Then
+            PictureBox1.Image = My.Resources.Dice4
+        ElseIf randomNumber = 5 Then
+            PictureBox1.Image = My.Resources.Dice5
+        Else
+            PictureBox1.Image = My.Resources.Dice6
+        End If
+
+        Dim randomNumber2 As Integer
+        Randomize()
+        randomNumber2 = Int(Rnd() * 6 + 1)
+
+        If randomNumber2 = 1 Then
+            PictureBox2.Image = My.Resources.Dice1
+        ElseIf randomNumber2 = 2 Then
+            PictureBox2.Image = My.Resources.Dice2
+        ElseIf randomNumber2 = 3 Then
+            PictureBox2.Image = My.Resources.Dice3
+        ElseIf randomNumber2 = 4 Then
+            PictureBox2.Image = My.Resources.Dice4
+        ElseIf randomNumber2 = 5 Then
+            PictureBox2.Image = My.Resources.Dice5
+        Else
+            PictureBox2.Image = My.Resources.Dice6
+        End If
+
+        Dim randomNumber3 As Integer
+        Randomize()
+        randomNumber3 = Int(Rnd() * 6 + 1)
+
+        If randomNumber3 = 1 Then
+            PictureBox3.Image = My.Resources.Dice1
+        ElseIf randomNumber3 = 2 Then
+            PictureBox3.Image = My.Resources.Dice2
+        ElseIf randomNumber3 = 3 Then
+            PictureBox3.Image = My.Resources.Dice3
+        ElseIf randomNumber3 = 4 Then
+            PictureBox3.Image = My.Resources.Dice4
+        ElseIf randomNumber3 = 5 Then
+            PictureBox3.Image = My.Resources.Dice5
+        Else
+            PictureBox3.Image = My.Resources.Dice6
+        End If
+
+
+
+        scoretemp += randomNumber + randomNumber2 + randomNumber3
+
+        If randomNumber = randomNumber2 Or randomNumber = randomNumber3 Or randomNumber2 = randomNumber3 Then
+            scoretemp = scoretemp * 2
+        End If
+
+
+        score += scoretemp
+
+        Label1.Text = "Score: " & score
+
+        If randomNumber = 1 And randomNumber2 = 1 Then
+            save(score)
+            endscript()
+        ElseIf randomNumber = 1 And randomNumber3 = 1
+            save(score)
+            endscript()
+        ElseIf randomNumber2 = 1 And randomNumber3 = 1
+            save(score)
+            endscript()
+        End If
+
+        If scoretemp > High_roll Then
+            High_roll = scoretemp
+        End If
+
+        Label2.Text = "Highest Roll: " & High_roll
+        Label3.Text = "High Score: " & save(score)
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        'Show "Form2.vb" '
+
+    End Sub
+End Class
